f388a68b82d1924374042ad30f46564332d0454d
Martin Alexandersson <martin.alexandersson@sspa.se>
2023-03-09 07:43:03 +0100

    safe commit.

--- src/phd/settings.py
+++ src/phd/settings.py
@@ -28,12 +28,17 @@ https://kedro.readthedocs.io/en/stable/kedro_project_setup/settings.html."""
 # CONFIG_LOADER_CLASS = ConfigLoader
 # Keyword arguments to pass to the `CONFIG_LOADER_CLASS` constructor.
 # CONFIG_LOADER_ARGS = {
-#       "config_patterns": {
-#           "spark" : ["spark*/"],
-#           "parameters": ["parameters*", "parameters*/**", "**/parameters*"],
-#       }
+#    "config_patterns": {
+#        "spark": ["spark*/"],
+#        "parameters": ["parameters*", "parameters*/**", "**/parameters*"],
+#    }
 # }
+from kedro.config import TemplatedConfigLoader  # new import
 
+CONFIG_LOADER_CLASS = TemplatedConfigLoader
+CONFIG_LOADER_ARGS = {
+    "globals_pattern": "*globals.yml",
+}
 # Class that manages the Data Catalog.
 # from kedro.io import DataCatalog
 # DATA_CATALOG_CLASS = DataCatalog
