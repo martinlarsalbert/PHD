41d7f452932dc549f5658a667c098be4c6634526
Martin Alexandersson <martin.alexandersson@sspa.se>
2023-11-07 13:27:05 +0100

    upgraded kedro version.

--- src/phd/settings.py
+++ src/phd/settings.py
@@ -3,9 +3,13 @@ from the Kedro defaults. For further information, including these default values
 https://kedro.readthedocs.io/en/stable/kedro_project_setup/settings.html."""
 
 # Instantiated project hooks.
-from phd.hooks import ProjectHooks
+# from phd.hooks import ProjectHooks
 
-HOOKS = (ProjectHooks(),)
+# HOOKS = (ProjectHooks(),)
+
+from kedro.config import OmegaConfigLoader  # noqa: import-outside-toplevel
+
+CONFIG_LOADER_CLASS = OmegaConfigLoader
 
 # Installed plugins for which to disable hook auto-registration.
 # DISABLE_HOOKS_FOR_PLUGINS = ("kedro-viz",)
@@ -34,19 +38,19 @@ HOOKS = (ProjectHooks(),)
 #        "parameters": ["parameters*", "parameters*/**", "**/parameters*"],
 #    }
 # }
-from kedro.config import TemplatedConfigLoader  # new import
-
-CONFIG_LOADER_CLASS = TemplatedConfigLoader
-# CONFIG_LOADER_ARGS = {
-#    "globals_pattern": "*globals.yml",
-# }
-# Class that manages the Data Catalog.
-# from kedro.io import DataCatalog
-# DATA_CATALOG_CLASS = DataCatalog
-
-from kedro_viz.integrations.kedro.sqlite_store import SQLiteStore
-from pathlib import Path
-
-SESSION_STORE_CLASS = SQLiteStore
-SESSION_STORE_ARGS = {"path": str(Path(__file__).parents[2] / "data")}
-# CONFIG_LOADER_ARGS = {"globals_pattern": "*globals.yml"}
+# from kedro.config import TemplatedConfigLoader  # new import
+#
+# CONFIG_LOADER_CLASS = TemplatedConfigLoader
+## CONFIG_LOADER_ARGS = {
+##    "globals_pattern": "*globals.yml",
+## }
+## Class that manages the Data Catalog.
+## from kedro.io import DataCatalog
+## DATA_CATALOG_CLASS = DataCatalog
+#
+# from kedro_viz.integrations.kedro.sqlite_store import SQLiteStore
+# from pathlib import Path
+#
+# SESSION_STORE_CLASS = SQLiteStore
+# SESSION_STORE_ARGS = {"path": str(Path(__file__).parents[2] / "data")}
+## CONFIG_LOADER_ARGS = {"globals_pattern": "*globals.yml"}
